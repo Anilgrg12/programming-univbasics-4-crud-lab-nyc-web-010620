ac448fff929bd47cfd2e03fa583b6bee8c9ed3ad
amusing-transpiler-2573 <anil_grg@hotmail.com>
2019-12-30 08:25:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,25 +1,29 @@
 def create_an_empty_array
+  [ ]
   
 end
 
-def create_an_array
+def create_an_array 
+  ["ram","sita","gita","prased"]
   
 end
 
 def add_element_to_end_of_array(array, element)
+  create_an_array.push ("arrays!")
   
 end
 
 def add_element_to_start_of_array(array, element)
+  create_an_array.unshift("wow")
   
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array! = create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  wow = create_an_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
